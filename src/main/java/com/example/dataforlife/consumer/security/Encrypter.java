a94b4aa4d5bb766e725327be3926a0b0057d18ff
ScNicolas <sirac.nicolas@gmail.com>
2018-07-07 14:46:10 +0200

    ReAdd encryption

--- src/main/java/com/example/dataforlife/consumer/security/Encrypter.java
+++ src/main/java/com/example/dataforlife/consumer/security/Encrypter.java
@@ -7,34 +7,15 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
 
 public class Encrypter {
 
     public static String encrypt(String plainText) throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-      /* // System.out.println(plainText);
-        String key = "ESGI4AL";
-
-
-        byte[] clean = plainText.getBytes();
-        int ivSize = 16;
-        byte[] iv = new byte[ivSize];
-        SecureRandom random = new SecureRandom();
-        random.nextBytes(iv);
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        digest.update(key.getBytes("UTF-8"));
-        byte[] keyBytes = new byte[16];
-        System.arraycopy(digest.digest(), 0, keyBytes, 0, keyBytes.length);
-        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
-        byte[] encrypted = cipher.doFinal(clean);
-        byte[] encryptedIVAndText = new byte[ivSize + encrypted.length];
-        System.arraycopy(iv, 0, encryptedIVAndText, 0, ivSize);
-        System.arraycopy(encrypted, 0, encryptedIVAndText, ivSize, encrypted.length);
-        return new String(encryptedIVAndText);*/
-      return plainText;
+        byte[] hash = digest.digest(plainText.getBytes(StandardCharsets.UTF_8));
+        return new String(hash);
     }
 
 }
